f6ec4226c36e41b55145306b8e40d9450b17a13e
ouko <oukoezzy@gmail.com>
2023-03-29 23:19:34 +0300

    add schema

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel")
 //@route GET /api/contacts
 const getContacts = asyncHandler(async (req,res) => {
-    res.status(200).json({ message: "Get all contacts" })
+    const contacts = await Contact.find();
+    res.status(200).json(contacts)
 });
 
 //@route POST /api/contacts
@@ -12,23 +14,54 @@ const createContact = asyncHandler(async (req,res) => {
         res.status(400);
         throw new Error("All fields required!")
     }
-    res.status(201).json({ message: "Create new contact" })
+
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    });
+
+    res.status(201).json(contact)
 });
 
 
 //@route GET /api/contacts/:id
 const getContact = asyncHandler(async (req,res) => {
-    res.status(200).json({ message: `Get contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //@route PUT /api/contacts/:id
 const updateContact = asyncHandler(async (req,res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("contact not found");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.status(200).json(updatedContact);
 });
 
 //@route DELETE /api/contacts/:id
 const deleteContact = asyncHandler(async (req,res) => {
-    res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("contact not found");
+    }
+    await Contact.remove();
+
+    res.status(200).json(contact);
 });
 
 
